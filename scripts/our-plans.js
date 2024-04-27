5856622bed11c997394eda18a225c7d0a120d742
FadyNF <fadyn2005@gmail.com>
2024-04-27 23:22:48 +0300

    Initial structure to "Plans" form

---
+++ scripts/our-plans.js
@@ -0,0 +1,34 @@
+(function() {
+    let currentPage = 1;
+    const prevBtn = document.querySelector(".form .form-footer .prev");
+    const nextBtn = document.querySelector(".form .form-footer .next");
+    
+    function movePage() {
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
+
+        if (currentPage === 1) {
+            prevBtn.disabled = true;
+        } else if (currentPage === 4) {
+            nextBtn.disabled = true;
+        }
+
+        document.querySelector(".form .pagination .active").classList.remove("active");
+        document.querySelectorAll(".form .pagination .number")[currentPage - 1].classList.add("active");
+    
+        const stepNode = document.querySelector(".form .steps .step");
+        const width = ((currentPage - 1) * stepNode.offsetWidth * -1) + "px";
+    
+        stepNode.parentNode.style.marginLeft = width;
+    }
+    
+    prevBtn.addEventListener("click", function(){
+        currentPage -= 1;
+        movePage();
+    });
+
+    nextBtn.addEventListener("click", function(){
+        currentPage += 1;
+        movePage();
+    });
+})();
