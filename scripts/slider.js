1c75714da3c6c30eead9ed1019c42c26569936ab
omneyaosama1 <omnia.osamahassan@gmail.com>
2024-04-27 20:24:17 +0300

    updating cookbook's functionalities

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,14 +1,56 @@
-const globalTabBox = document.querySelector(".cuisines .tabs-bar");
-const globalArrowIcon = document.querySelectorAll(".cuisines .icon i");
-const dishesTabBox = document.querySelector(".dishes .tabs-bar");
-const dishesArrowIcon = document.querySelectorAll(".dishes .icon i");
-globalArrowIcon.forEach((icon) => {
+// Handling cuisine tabs
+const cuisineTabBox = document.querySelector(".cuisines .tabs-bar");
+const cuisineArrowIcons = document.querySelectorAll(".cuisines .icon i");
+
+const handleGlobalIcon = () => {
+    let scrollVal = Math.round(cuisineTabBox.scrollLeft);
+    let maxScrollWidth = cuisineTabBox.scrollWidth - cuisineTabBox.clientWidth;
+    cuisineArrowIcons[0].parentElement.style.display =
+      scrollVal > 0 ? "flex" : "none";
+    cuisineArrowIcons[1].parentElement.style.display =
+      maxScrollWidth > scrollVal ? "flex" : "none";
+  };
+  
+
+
+cuisineArrowIcons.forEach((icon) => {
   icon.addEventListener("click", () => {
-    globalTabBox.scrollLeft += icon.id === "left-cuisines" ? -350 : 350;
+    cuisineTabBox.scrollLeft += icon.id === "left-cuisine" ? -350 : 350;
+    setTimeout(()=>handleGlobalIcon(),50);
   });
 });
-dishesArrowIcon.forEach((icon) => {
+
+const cuisineTabs = document.querySelectorAll(".cuisines .tab");
+
+cuisineTabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tab.classList.toggle("active");
+  });
+});
+
+// Handling dishes tabs
+const dishesTabBox = document.querySelector(".dishes .tabs-bar");
+const dishesArrowIcons = document.querySelectorAll(".dishes .icon i");
+const handleDishIcon = () => {
+    let scrollVal = Math.round(dishesTabBox.scrollLeft);
+    let maxScrollWidth = dishesTabBox.scrollWidth - dishesTabBox.clientWidth;
+    dishesArrowIcon[0].parentElement.style.display =
+      scrollVal > 0 ? "flex" : "none";
+    dishesArrowIcon[1].parentElement.style.display =
+      maxScrollWidth > scrollVal ? "flex" : "none";
+  };
+  
+dishesArrowIcons.forEach((icon) => {
   icon.addEventListener("click", () => {
     dishesTabBox.scrollLeft += icon.id === "left-dishes" ? -350 : 350;
+    setTimeout(()=>handleDishIcon(),50);
+  });
+});
+
+const dishesTabs = document.querySelectorAll(".dishes .tab");
+
+dishesTabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tab.classList.toggle("active");
   });
 });
