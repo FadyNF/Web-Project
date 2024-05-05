d49147c1dad8502f18e58152b1e625106b4b33d9
YoumnaHanyy <153848957+YoumnaHanyy@users.noreply.github.com>
2024-05-05 20:29:25 +0300

    Add files via upload
    i added to the navigation bar when i scroll the background get darker with js

--- Recipe.js
+++ Recipe.js
@@ -157,3 +157,17 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+
+    window.addEventListener('scroll', function() {
+        var navbar = document.querySelector('.nav-bar');
+        if (window.scrollY > 0) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
+
+
+
+
