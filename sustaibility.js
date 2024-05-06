53852efac678636530335ee81d477d86a239900c
SarahWaelElemam <153671228+SarahWaelElemam@users.noreply.github.com>
2024-05-06 12:14:53 +0000

    removed unnessecary extra functionalities in js and fixed a typo in html

--- sustaibility.js
+++ sustaibility.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Select the discover button
   const discoverButton = document.querySelector(".discover-button");
 
-  // Add event listener for button click
   discoverButton.addEventListener("click", function() {
-    // Navigate to the new page
     window.location.href = "sourcing.html";
   });
 
