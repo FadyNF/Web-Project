50f886e9b6bb2e2f7ed0a63c69bbf0afd6732a8c
FadyNF <fadynf05@gmail.com>
2024-05-09 05:30:01 +0300

    fixed bugs in cookbook

--- scripts/user.js
+++ scripts/user.js
@@ -1,117 +1,123 @@
+const navBar = document.querySelector("nav"),
+  sidebar = document.querySelectorAll(".menu-icon"),
+  overlay = document.querySelector(".overlay");
+
+sidebar.forEach((button) => {
+  button.addEventListener("click", () => {
+    navBar.classList.toggle("open");
+  });
+});
+
+overlay.addEventListener("click", () => {
+  navBar.classList.remove("open");
+});
 
 
-function showAccount() {
-    var div = document.getElementById('account-content');
-    div.style.display = "block";
+const infoEditBtn = document.getElementById("infoEdit");
+const addressEditBtn = document.getElementById("addressEdit");
+const cardEditBtn = document.getElementById("cardEdit");
+const passwordEditBtn = document.getElementById("passwordEdit");
 
-    var historydiv = document.getElementById('history-content');
-historydiv.style.display="none";
+const userEditOverlay = document.getElementById("userEditOverlay");
+const addressEditOverlay = document.getElementById("addressEditOverlay");
+const cardEditOverlay = document.getElementById("cardEditOverlay");
+const passwordEditOverlay = document.getElementById("passwordEditOverlay");
+
+
+infoEditBtn.addEventListener("click", function () {
+  userEditOverlay.style.display = "block";
+});
+
+addressEditBtn.addEventListener("click", function () {
+  addressEditOverlay.style.display = "block";
+});
+
+cardEditBtn.addEventListener("click", function () {
+  cardEditOverlay.style.display = "block";
+});
+passwordEditBtn.addEventListener("click", function () {
+  passwordEditOverlay.style.display = "block";
+});
+
+document.addEventListener("click", function (event) {
+  if (
+    !event.target.closest(".overlay-content") &&
+    !event.target.closest(".edit-btn")
+  ) {
+    userEditOverlay.style.display = "none";
+    addressEditOverlay.style.display = "none";
+    cardEditOverlay.style.display = "none";
+  }
+});
+
+/********************************************************************/
+function editUserInfo() {
+
+  var firstName = document.getElementById("fname").innerText;
+  var lastName = document.getElementById("lname").innerText;
+  var email = document.getElementById("email").innerText;
+  var phone = document.getElementById("phone").innerText;
+
+  document.getElementById("editFirstName").value = firstName;
+  document.getElementById("editLastName").value = lastName;
+  document.getElementById("editEmail").value = email;
+  document.getElementById("editPhone").value = phone;
+
+  document.getElementById("userEditOverlay").style.display = "block";
 }
 
+function saveUserInfo() {
 
-function showHistory() {
-    var div = document.getElementById('history-content');
-    div.style.display = "block";
+  var editedFirstName = document.getElementById("editFirstName").value;
+  var editedLastName = document.getElementById("editLastName").value;
+  var editedEmail = document.getElementById("editEmail").value;
+  var editedPhone = document.getElementById("editPhone").value;
 
-    var accountdiv = document.getElementById('account-content');
-    accountdiv.style.display = "none";
+  document.getElementById("fname").innerText = editedFirstName;
+  document.getElementById("lname").innerText = editedLastName;
+  document.getElementById("email").innerText = editedEmail;
+  document.getElementById("phone").innerText = editedPhone;
+
+  document.getElementById("userEditOverlay").style.display = "none";
 }
 
-// showing edit text fields
-function showtextField(fieldID) {
-    var field = document.getElementById(fieldID);
-    field.style.display = "inline-block";
+function editAddressInfo() {
+  var country = document.getElementById("country").innerText;
+  var city = document.getElementById("city").innerText;
+
+  document.getElementById("editCountry").value = country;
+  document.getElementById("editCity").value = city;
+
+  document.getElementById("addressEditOverlay").style.display = "block";
 }
 
-function printWarning(elementID, hintMSG) {
-    document.getElementById(elementID).innerHTML = hintMSG;
+function saveAddressInfo() {
+  var editedCountry = document.getElementById("editCountry").value;
+  var editedCity = document.getElementById("editCity").value;
+
+  document.getElementById("country").innerText = editedCountry;
+  document.getElementById("city").innerText = editedCity;
+
+  document.getElementById("addressEditOverlay").style.display = "none";
 }
 
-
-function validation(editField, originalField, spanID) {
-    var newInput = document.querySelector('input[name="' + editField + '"]');
-
-    var nameRegex = /^[a-zA-Z\s]+$/;
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    var numberRegex = /^(?:\+?88)?01[0-9]{9}$/;
-
-    if (editField === 'editFname' || editField === 'editLname') {
-        if (newInput.value.trim() === "") {
-            printWarning(editField + "-err", "please enter an input");
-        }
-        else if (!nameRegex.test(newInput.value)) {
-            printWarning(editField + "-err", "please enter a valid input.");
-        }
-        else {
-            printWarning(editField + "-err", "");
-            edit(editField, originalField, spanID);
-        }
-    }
-
-    else if (editField === 'editEmail') {
-        if (newInput.value.trim() === "") {
-            printWarning(editField + "-err", "please enter an input");
-        }
-        else if (!emailRegex.test(newInput.value)) {
-            printWarning(editField + "-err", "please enter a valid input.");
-        }
-        else {
-            printWarning(editField + "-err", "");
-            edit(editField, originalField, spanID);
-        }
-    }
-    else if (editField === 'editNumber') {
-        if (newInput.value.trim() === "") {
-            printWarning(editField + "-err", "please enter an input");
-        }
-        else if (!numberRegex.test(newInput.value)) {
-            printWarning(editField + "-err", "please enter a valid input.");
-        }
-        else {
-            printWarning(editField + "-err", "");
-            edit(editField, originalField, spanID);
-        }
-    }
-    else {
-        if (newInput.value.trim() === "") {
-            printWarning(editField + "-err", "please enter an input");
-        }
-        else {
-            printWarning(editField + "-err", "");
-            edit(editField, originalField, spanID);
-        }
-    }
+function editPasswordInfo() {
+  var password = document.getElementById("password");
+  document.getElementById("editPassword").value = password;
+  document.getElementById("passwordEditOverlay").style.display = "block";
 }
-
-
-
-function edit(editfield, originfield, spanID) {
-    var originalField = document.querySelector('input[name="' + originfield + '"]');
-    var editedPart = document.querySelector('input[name="' + editfield + '"]');
-
-    originalField.value = editedPart.value;
-
-    var span = document.getElementById(spanID);
-    if (span) {
-        span.style.display = "none";
-    }
-
-    showMessage();
+function savePassword() {
+  var newPassword = document.getElementById("editPassword").value;
+  document.getElementById("password").innerText = newPassword;
+  document.getElementById("passwordEditOverlay").style.display = "none";
 }
-
-
-// after editting
-function showMessage() {
-    var msg = document.querySelectorAll('.message');
-    msg.forEach(messg => {
-        messg.style.display = "block";
-    });
-    setTimeout(closeMessage, 6000);
-
+function editCardInfo() {
+  var password = document.getElementById("card");
+  document.getElementById("editCardNum").value = password;
+  document.getElementById("cardEditOverlay").style.display = "block";
 }
-
-function closeMessage() {
-    var message = document.querySelector('.message');
-    message.style.display = "none";
-    setTimeout(closeMessage, 3000);
+function saveCardInfo() {
+  var newPassword = document.getElementById("editCardNum").value;
+  document.getElementById("card").innerText = newPassword;
+  document.getElementById("cardEditOverlay").style.display = "none";
 }
